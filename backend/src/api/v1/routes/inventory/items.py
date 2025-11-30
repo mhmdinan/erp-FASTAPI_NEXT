370bec133a1c5078f3b12d310f1dd52ecf95f7b8
Inan <muhammadinan41@gmail.com>
2025-11-30 15:20:48 +0500

    add item apis

--- backend/src/api/v1/routes/inventory/items.py
+++ backend/src/api/v1/routes/inventory/items.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from crud import item as item_crud
 from db.db import get_db
 from sqlalchemy.orm import Session
@@ -13,4 +13,51 @@ def item_router_root():
 @item_router.post("/create-item")
 def item_router_create_item(item_create: item_schema.ItemCreate, db: Session = Depends(get_db)):
     item = item_crud.create_item(db, item_create)
+    return item
+
+@item_router.get("/get-item-byname/{item-name}")
+def item_router_get_item_byname(item_name: str, db: Session = Depends(get_db)):
+    item = item_crud.get_item(db, item_name)
+    if item is None:
+        raise HTTPException(status_code=404, detail="item not found")
+    return item
+
+@item_router.get("/get-item-bysky/{item-sku}")
+def item_router_get_item_bysku(item_sku: str, db: Session = Depends(get_db)):
+    item = item_crud.get_item_by_sku(db, item_sku)
+    if item is None:
+        raise HTTPException(status_code=404, detail="item not found")
+    return item
+
+@item_router.get("/get-item-byid/{item-id}")
+def item_router_get_item_byid(item_id: int, db: Session = Depends(get_db)):
+    item = item_crud.get_item_by_id(db, item_id)
+    if item is None:
+        raise HTTPException(status_code=404, detail="item not found")
+    return item
+
+@item_router.put("/update-item")
+def item_router_update_item(item_to_update: item_schema.ItemUpdate, db: Session = Depends(get_db)):
+    item = item_crud.update_item(db, item_to_update)
+    if item is None:
+        raise HTTPException(status_code=404, detail="item not found")
+    return item
+
+@item_router.get("/get-items")
+def item_router_get_items(search_settings: item_schema.ItemsList, db: Session = Depends(get_db)):
+    items, total = item_crud.get_items(db, search_settings)
+    return [items, total]
+
+@item_router.put("/deactivate-item/{item-name}")
+def item_router_deactivate_item(item_name: str, db: Session = Depends(get_db)):
+    item = item_crud.deactivate_item(db, item_name)
+    if item is None:
+        raise HTTPException(status_code=404, detail="item not found")
+    return item
+
+@item_router.put("/activate-item/{item-name}")
+def item_router_activate_item(item_name: str, db: Session = Depends(get_db)):
+    item = item_crud.activate_item(db, item_name)
+    if item is None:
+        raise HTTPException(status_code=404, detail="item not found")
     return item
