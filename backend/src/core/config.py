e805cbf4040bccec68e418a6f22b1c664bd2de88
mhmdinan <muhammadinan41@gmail.com>
2026-02-01 22:24:52 +0500

    backend: added config.py to get settings from env file. frontend: started fresh fronend development using vite + react framework

---
+++ backend/src/core/config.py
@@ -0,0 +1,10 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "ERP System"
+
+    model_config = SettingsConfigDict(
+        env_file="../.env",
+        env_file_encoding="utf-8",
+        case_sensitive=True
+    )
---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-zinc-900">
+      <h1 className="text-4xl font-bold text-cyan-400">
+        ERP Frontend Connected
+      </h1>
+    </div>
+  )
+}
+
+export default App
