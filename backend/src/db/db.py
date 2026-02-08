3fcc7505d52dd21baeb6e5c5c4cc8f41db514a26
mhmdinan <muhammadinan41@gmail.com>
2026-02-08 15:52:37 +0500

    switch db engine to async type

--- backend/src/db/db.py
+++ backend/src/db/db.py
@@ -1,12 +1,15 @@
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio.session import AsyncSession
 from sqlalchemy.orm import sessionmaker
 from db.base import Base
-from db.models import department , user, item, employee
+from db.models import department, user, item, employee
 
-#TODO: Add db currently using placeholder
-DB_URL="sqlite:///erp.db"
-engine = create_engine(DB_URL)
-SessionLocal = sessionmaker(autoflush=False, bind=engine)
+# TODO: Add db currently using placeholder
+DB_URL = "sqlite:///erp.db"
+engine = create_async_engine(DB_URL)
+SessionLocal = sessionmaker(
+    autoflush=False, autocommit=False, bind=engine, class_=AsyncSession, expire_on_commit= False
+)
 
 
 def get_db():
